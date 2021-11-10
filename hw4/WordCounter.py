d42240f6c468ef5326e14d605f6e03a5545d9bc7
Yunseo-Han <57244943+Yunseo-Han@users.noreply.github.com>
2021-11-09 23:19:56 -0800

    Unique word dictionary added

--- hw4/WordCounter.py
+++ hw4/WordCounter.py
@@ -1,30 +1,57 @@
 import csv
 import re
+import copy
 from itertools import islice
 
 
 class WordCounter:
 
-    def __init__(self, filename):
-        self.filename = filename
-        self.file = open(filename)
+    def __init__(self, posFilename, negFilename):
+        self.posfile = open(posFilename)
+        self.negfile = open(negFilename)
+        self.posdict = {}
+        self.negdict = {}
 
-    def fileIterator(self, isPos):
-        if(isPos):
-            reader = csv.reader(islice(self.file, start=2, stop=812))
-        else:
-            reader = csv.reader(islice(self.file, start=812, stop=None))
+    def createDict(self):
+        posReader = csv.reader(self.posfile)
+        negReader = csv.reader(self.negfile)
 
-        for row in reader:
+        words = []
+        for row in islice(posReader, 2, None):
             # do something
-            self.textParser(row[2])
+            words += self.textParser(row[2])
+        for row in islice(negReader, 2, None):
+            # do something
+            words += self.textParser(row[2])
 
-    def textParser(text):
+        for word in words:
+            self.posdict[word] = 0
+        self.negdict = copy.deepcopy(self.posdict)
+
+    def textParser(self, text):
         parsed = re.split(r'[^\w]', text)
         return parsed
 
-    def wordCounter(parsedText, word):
-        counter = 0
-        for e in parsedText:
-            if e == word:
-                counter += 1
+    def wordCounter(self):
+        posReader = csv.reader(self.posfile)
+        negReader = csv.reader(self.negfile)
+
+        for row in islice(posReader, 2, None):
+            parsed = self.textParser(row[2])
+            for word in parsed:
+                if self.posdict.has_key(word):
+                    self.posdict[word] += 1
+
+        for row in islice(negReader, 2, None):
+            parsed = self.textParser(row[2])
+            for word in parsed:
+                if self.negdict.has_key(word):
+                    self.negdict[word] += 1
+
+
+test = WordCounter("./pos_train.csv", "./neg_train.csv")
+test.createDict()
+print(test.posdict)
+print("****************************************************************************************")
+test.wordCounter()
+print(test.posdict)
